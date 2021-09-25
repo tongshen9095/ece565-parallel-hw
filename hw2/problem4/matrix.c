0cefa2e218eff3fdfaab8e1dd4cc63b0b9d995fe
tong shen <ts325@duke.edu>
2021-09-25 19:43:07 -0400

    init matrix

--- hw2/problem4/matrix.c
+++ hw2/problem4/matrix.c
@@ -5,21 +5,31 @@
 
 # define N 1024
 
-double calc_time(struct timespec start, struct timespec end) { 
-  double start_sec = (double)start.tv_sec*1000000000.0 + (double)start.tv_nsec; 
-  double end_sec = (double)end.tv_sec*1000000000.0 + (double)end.tv_nsec; 
-  if (end_sec < start_sec) {  
-    return 0;   
-  } else {   
-    return end_sec - start_sec;
-  } 
+double calc_time(struct timespec start, struct timespec end) {
+    double start_sec = (double) start.tv_sec * 1000000000.0 + (double) start.tv_nsec;
+    double end_sec = (double) end.tv_sec * 1000000000.0 + (double) end.tv_nsec;
+    if (end_sec < start_sec) {
+        return 0;
+    } else {
+        return end_sec - start_sec;
+    }
 }
 
 
+void initMatrix(double matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = rand() % N;
+        }
+    }
 
-void initMatrix(double M)
+}
 
 int main(int argc, char *argv[]) {
-  double A[N][N] B[N][N] C[N][N];
-  return EXIT_SUCCESS;
+    double A[N][N];
+    double B[N][N];
+    double C[N][N];
+    initMatrix(A);
+    initMatrix(B);
+    return EXIT_SUCCESS;
 }
