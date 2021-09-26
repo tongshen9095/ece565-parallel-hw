616edefc7423ea3a1a23f10cd3b38d63214cac36
tongshen9095 <tongshen9095@gmail.com>
2021-09-26 00:33:05 -0400

    use vector

--- hw2/problem4/matrix.cpp
+++ hw2/problem4/matrix.cpp
@@ -1,33 +1,24 @@
 #include <stdio.h>
-#include <sys/time.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <sys/time.h>
 #include <iostream>
 #include <vector>
 
 # define N 1024
 
-double ** initMatrix() {
-  double ** matrix = new double * [N];
-  for (int i = 0; i < N; i++) {
-    matrix[i] = new double[N];
-  }
+std::vector<std::vector<double>> initMatrix() {
+  std::vector<std::vector<double> > matrix(N, std::vector<double>(N, 0));
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       matrix[i][j] = rand() % N;
     }
   }
-}
-
-void freeMatrix(double ** matrix) {
-  for (int i = 0; i < N; i++) {
-    delete[] matrix[i];
-  }
-  delete[] matrix;
+  return matrix;
 }
 
 int main() {
-  double ** A = initMatrix();
-  freeMatrix(A);
+  std::vector<std::vector<double>> A = initMatrix();
+  std::vector<std::vector<double>> B = initMatrix();
+  
   return EXIT_SUCCESS;;
 }
--- hw2/problem4/matrix_copy.c
+++ hw2/problem4/matrix_copy.c
@@ -8,8 +8,6 @@
 double calc_time(struct timespec start, struct timespec end) {
     double start_sec = (double) start.tv_sec * 1000000000.0 + (double) start.tv_nsec;
     double end_sec = (double) end.tv_sec * 1000000000.0 + (double) end.tv_nsec;
-    printf("%f\n", start_sec);
-    printf("%f\n", end_sec);
     if (end_sec < start_sec) {
         return 0;
     } else {
@@ -27,15 +25,12 @@ void initMatrix(double matrix[N][N]) {
 
 // i -> j -> k
 void multiply_i(double A[N][N], double B[N][N], double C[N][N]) {
-    printf("%s", "start");
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             double sum = 0;
             for (int k = 0; k < N; k++) {
-                printf("%f\n", sum);
                 sum += A[i][k] * B[k][j];
             }
-            printf("%f\n", sum);
             C[i][j] = sum;
         }
     }
@@ -49,7 +44,6 @@ int main(int argc, char *argv[]) {
     initMatrix(A);
     initMatrix(B);
 
-    printf("%s", "s1");
     struct timespec start_time, end_time;
     clock_gettime(CLOCK_MONOTONIC, &start_time);
     multiply_i(A, B, C);
