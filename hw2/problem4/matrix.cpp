8a258a3170d9f8afa93b558c98f09d5045f30d67
tongshen9095 <tongshen9095@gmail.com>
2021-09-26 00:57:25 -0400

    add mutiplyI

--- hw2/problem4/matrix.cpp
+++ hw2/problem4/matrix.cpp
@@ -6,7 +6,7 @@
 
 # define N 1024
 
-std::vector<std::vector<double>> initMatrix() {
+std::vector<std::vector<double> > initMatrix() {
   std::vector<std::vector<double> > matrix(N, std::vector<double>(N, 0));
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -16,9 +16,22 @@ std::vector<std::vector<double>> initMatrix() {
   return matrix;
 }
 
+void multiplyI(std::vector<std::vector<double>> A,
+	       std::vector<std::vector<double>> B) {
+  std::vector<std::vector<double> > C(N, std::vector<double>(N, 0));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; k++) {
+	C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  std::cout << "C[N - 1][N - 1] = " << C[N - 1][N - 1] << "\n";
+}
+
 int main() {
   std::vector<std::vector<double>> A = initMatrix();
   std::vector<std::vector<double>> B = initMatrix();
-  
+  multiplyI(A, B);
   return EXIT_SUCCESS;;
 }
