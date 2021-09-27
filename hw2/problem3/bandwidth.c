104e191781080aed9c0ef09cdc0fb1cb86ed1c6d
tongshen9095 <tongshen9095@gmail.com>
2021-09-27 11:14:18 -0400

    change num_elements

--- hw2/problem3/bandwidth.c
+++ hw2/problem3/bandwidth.c
@@ -45,14 +45,18 @@ int main(int argc, char * argv[]) {
   }
   
   int num_elements = atoi(argv[1]);
-  
-  if (num_elements != 4000 && num_elements != 4000000) {
-    printf("The number of elements can only be 4000 or 4000000\n");
-    return EXIT_FAILURE;
-  }
-  
   int num_iters = atoi(argv[2]);
   int choice = atoi(argv[3]);
+  
+  if (num_elements != 4096 && num_elements != 4000000) {
+    printf("The number of elements can only be 4096 or 4000000\n");
+    return EXIT_FAILURE;
+  }
+
+  if (choice != 1 && choice != 2 && choice != 3) {
+    printf("The choice can only be 1, 2 or 3\n");
+    return EXIT_FAILURE;
+  }
   
   uint64_t * array = (uint64_t*)malloc(num_elements * sizeof(uint64_t));
   for (int index = 0; index < num_elements; index++) {
