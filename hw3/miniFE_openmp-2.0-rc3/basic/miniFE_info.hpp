e917d1efa7611c9826fbe4a31998907efcafe1f1
tong <ts325@duke.edu>
2021-10-13 11:42:34 -0400

    modify makefile

---
+++ hw3/miniFE_openmp-2.0-rc3/basic/miniFE_info.hpp
@@ -0,0 +1,13 @@
+#ifndef miniFE_info_hpp
+#define miniFE_info_hpp
+
+#define MINIFE_HOSTNAME "kraken"
+#define MINIFE_KERNEL_NAME "'Linux'"
+#define MINIFE_KERNEL_RELEASE "'5.11.0-36-generic'"
+#define MINIFE_PROCESSOR "'x86_64'"
+
+#define MINIFE_CXX "'/usr/bin/g++'"
+#define MINIFE_CXX_VERSION "'g++ (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0'"
+#define MINIFE_CXXFLAGS "'-g -pg -O3 -fno-inline'"
+
+#endif
